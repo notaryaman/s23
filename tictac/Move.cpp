05b3ca687ef51dac78b3d3b9d6bdd777d26dbdc8
notaryaman <124849392+notaryaman@users.noreply.github.com>
2023-04-25 04:19:55 -0700

    Update Move.cpp

--- tictac/Move.cpp
+++ tictac/Move.cpp
@@ -1,12 +1,59 @@
 #include "Errors.h"
 #include "Move.h"
-
+#include <sstream>
+using namespace std;
 // Space for implementing Move functions.
 
-Move::Move(const std::string& input) {
-
+Move::Move(const std::string& input)
+{
+  std::istringstream in(input);
+  in>>number;
+  in>>player;
+  player = toupper(player);
+  in>>row;
+  in>>column;
+  if(number>9||number<1)
+  {
+          throw ParseError("Invalid number");
+  }
+  if(player == 'O' || player == 'X')
+  {
+          player = player;
+  }
+  else
+  {
+          throw ParseError("Invalid player");
+  }
+  if(row>='A' && row<='C')
+  {
+          row = (row - 'A') + 1;
+  }
+   else
+  {
+          throw ParseError("Invalid row");
+  }
+  if(column>="1"&&column<="3")
+  {
+       column = column;
+  }
+  else
+  {
+          throw ParseError("Invalid column");
+  }
+  if (!iss.eof())
+  {
+    char nextChar;
+    in >> nextChar;
+    if (nextChar != '#')
+    {
+        throw ParseError("Invalid after column");
+    }
+  }
 }
 
-std::ostream& operator << (std::ostream& stream, const Move& move) {
-
+std::ostream& operator << (std::ostream& stream, const Move& move) 
+{
+  stream<<move.number<<' '<<move.player<<' ';
+  stream<< <char>row<< <char>column;
+  return stream;
 }
