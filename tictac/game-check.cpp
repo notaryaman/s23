e098e1d00e529205e413b648881a165aaaf9abae
notaryaman <124849392+notaryaman@users.noreply.github.com>
2023-04-25 05:35:00 -0700

    Update game-check.cpp

--- tictac/game-check.cpp
+++ tictac/game-check.cpp
@@ -1,44 +1,43 @@
 #include "Board.h"
 #include "Errors.h"
-#include "Move.h"
 #include <iostream>
-int main() 
-{
-  std::vector<std::vector<char>> board(3, std::vector<char>(3, ' '));
+#include "Move.h"
+
+int main() {
+    Board board;
     std::string line;
-    int moves = 0;
+    char nextPlayer = 'X';
 
     while (std::getline(std::cin, line)) {
         try {
             Move move(line);
-            if (board[move.row][move.column] != ' ') {
+            if (!board.applyMove(move)) {
                 std::cout << "Invalid move." << std::endl;
                 return 2;
             }
 
-            board[move.row][move.column] = move.player;
-            ++moves;
-
-            if (checkWin(board, move.player)) {
+            if (board.isWinningMove(move)) {
                 std::cout << "Game over: " << move.player << " wins." << std::endl;
                 return 0;
             }
 
-            if (moves == 9) {
+            if (board.isFull()) {
                 std::cout << "Game over: Draw." << std::endl;
                 return 0;
             }
 
-        } catch (const ParseError& e) {
+            nextPlayer = (move.player == 'X') ? 'O' : 'X';
+        }
+        catch (const ParseError&) {
             std::cout << "Parse error." << std::endl;
             return 1;
         }
     }
 
-    if (moves == 0) {
+    if (nextPlayer == 'X' && line.empty()) {
         std::cout << "Game in progress: New game." << std::endl;
-    } else {
-        char nextPlayer = (moves % 2 == 0) ? 'X' : 'O';
+    }
+    else {
         std::cout << "Game in progress: " << nextPlayer << "'s turn." << std::endl;
     }
 
