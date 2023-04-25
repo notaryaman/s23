4b2e2d629268470c7bfc90be398cfcf12befc0e8
notaryaman <124849392+notaryaman@users.noreply.github.com>
2023-04-25 04:38:38 -0700

    Update Move.cpp

--- tictac/Move.cpp
+++ tictac/Move.cpp
@@ -2,58 +2,64 @@
 #include "Move.h"
 #include <sstream>
 using namespace std;
-// Space for implementing Move functions.
 
 Move::Move(const std::string& input)
 {
-  std::istringstream in(input);
-  in>>number;
-  in>>player;
-  player = toupper(player);
-  in>>row;
-  in>>column;
-  if(number>9||number<1)
-  {
-          throw ParseError("Invalid number");
-  }
-  if(player == 'O' || player == 'X')
-  {
-          player = player;
-  }
-  else
-  {
-          throw ParseError("Invalid player");
-  }
-  if(row>='A' && row<='C')
-  {
-          row = (row - 'A') + 1;
-  }
-   else
-  {
-          throw ParseError("Invalid row");
-  }
-  if(col>=1 &&col<= 3)
-  {
-       column = column;
-  }
-  else
-  {
-          throw ParseError("Invalid column");
-  }
-  if (!in.eof())
-  {
-    char nextChar;
-    in >> nextChar;
-    if (nextChar != '#')
+    std::istringstream in(input);
+    in >> number;
+    in >> player;
+    player = toupper(player);
+
+    char rowChar, colChar;
+    in >> rowChar >> colChar;
+    int col = colChar - '0';
+
+    if (number > 9 || number < 1) 
     {
-        throw ParseError("Invalid after column");
+        throw ParseError("Invalid number");
     }
-  }
+
+    if (player == 'O' || player == 'X') 
+    {
+        player = player;
+    } 
+     else 
+    {
+        throw ParseError("Invalid player");
+    }
+
+    if (rowChar >= 'A' && rowChar <= 'C') 
+    {
+        row = rowChar - 'A';
+    } 
+    else
+    {
+        throw ParseError("Invalid row");
+    }
+
+    if (col >= 1 && col <= 3)
+    {
+        column = col - 1;
+    } 
+  else
+  {
+        throw ParseError("Invalid column");
+    }
+
+    if (!in.eof())
+    {
+        char nextChar;
+        in >> nextChar;
+        if (nextChar != '#') 
+        {
+            throw ParseError("Invalid after column");
+        }
+    }
 }
 
-std::ostream& operator << (std::ostream& stream, const Move& move) 
+std::ostream& operator<<(std::ostream& stream, const Move& move)
 {
-  stream<<move.number<<' '<<move.player<<' ';
-  stream<< move.row<< move.column;
-  return stream;
+    stream << move.number << ' ' << move.player << ' ';
+    stream << static_cast<char>('A' + move.row) << static_cast<char>('1' + move.column);
+    return stream;
 }
