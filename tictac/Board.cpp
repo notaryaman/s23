6165708deb2a34e7051e26ee2349dd388b815c2c
notaryaman <124849392+notaryaman@users.noreply.github.com>
2023-04-25 05:26:57 -0700

    Update Board.cpp

--- tictac/Board.cpp
+++ tictac/Board.cpp
@@ -1,71 +1,47 @@
-#include "Errors.h"
-#include "Board.h"
 #include <iostream>
 #include <vector>
 #include <string>
 #include "Move.h"
-// Space for implementing Board functions.
+#include "Errors.h"
 
-Board::Board() : board_(3, std::vector<char>(3, ' ')) {}
-
-bool Board::applyMove(const Move& move) {
-    if (board_[move.row][move.column] != ' ') {
-        return false;
-    }
-    board_[move.row][move.column] = move.player;
-    return true;
-}
-
-bool Board::isWinningMove(const Move& move) const {
-    char player = move.player;
-
+bool checkWin(const std::vector<std::vector<char>>& board, char player) {
     for (int i = 0; i < 3; ++i) {
-        if ((board_[i][0] == player && board_[i][1] == player && board_[i][2] == player) ||
-            (board_[0][i] == player && board_[1][i] == player && board_[2][i] == player)) {
+        if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
+            (board[0][i] == player && board[1][i] == player && board[2][i] == player)) {
             return true;
         }
     }
 
-    if ((board_[0][0] == player && board_[1][1] == player && board_[2][2] == player) ||
-        (board_[0][2] == player && board_[1][1] == player && board_[2][0] == player)) {
+    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+        (board[0][2] == player && board[1][1] == player && board[2][0] == player)) {
         return true;
     }
 
     return false;
 }
 
-bool Board::isFull() const {
-    for (const auto& row : board_) {
-        for (char cell : row) {
-            if (cell == ' ') {
-                return false;
-            }
-        }
-    }
-    return true;
-}
 int main() {
-    Board board;
+    std::vector<std::vector<char>> board(3, std::vector<char>(3, ' '));
     std::string line;
     int moves = 0;
 
     while (std::getline(std::cin, line)) {
         try {
             Move move(line);
-
-            if (!board.applyMove(move)) {
+            if (board[move.row][move.column] != ' ') {
                 std::cout << "Invalid move." << std::endl;
                 return 2;
             }
 
+            board[move.row][move.column] = move.player;
             ++moves;
 
-            if (board.isWinningMove(move)) {
+            if (checkWin(board, move.player)) {
                 std::cout << "Game over: " << move.player << " wins." << std::endl;
                 return 0;
             }
 
-            if (board.isFull()) {
+            if (moves == 9) {
                 std::cout << "Game over: Draw." << std::endl;
                 return 0;
             }
