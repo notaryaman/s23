5871edb0ade7785c43f66688ab64a18c38650059
notaryaman <124849392+notaryaman@users.noreply.github.com>
2023-04-25 05:26:17 -0700

    Update Board.h

--- tictac/Board.h
+++ tictac/Board.h
@@ -5,14 +5,5 @@
 
 // I recommended writing a Board class to manage your game state.
 // Here's some space for the class definition; member functions go in Board.cpp.
-class Board {
-public:
-    Board();
-    bool applyMove(const Move& move);
-    bool isWinningMove(const Move& move) const;
-    bool isFull() const;
 
-private:
-    std::vector<std::vector<char>> board_;
-};
 #endif
