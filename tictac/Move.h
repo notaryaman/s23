d0a4072b0108a73dcb93f4c471b01913bdbd4b4e
notaryaman <124849392+notaryaman@users.noreply.github.com>
2023-04-27 01:52:21 -0700

    Update Move.h

--- tictac/Move.h
+++ tictac/Move.h
@@ -1,27 +1,19 @@
 #ifndef MOVE_H
 #define MOVE_H
 
-// This file defines a Move structure and some helper functions.
-// You can edit it if you feel like it, but you shouldn't need to.
-// You're free to interpret the member variables as you wish,
-// but you must print moves in the expected format.
-
-#include <ostream>
 #include <string>
 
-struct Move {
-  // Member Variables
-  int  number;
-  char player;
-  int  row;
-  int  column;
+class Move {
+public:
+    Move(const std::string& line);
 
-  // The constructor parses a string.
-  Move(const std::string& input);
+    int moveNumber;
+    char player;
+    int row;
+    int column;
+
+    bool isValid(char expectedPlayer) const;
 };
 
-// Adding a << operator allows easy printing to std::cout.
-// This should print the Move in the format described in the readme.
-std::ostream& operator << (std::ostream& stream, const Move& move);
+#endif // MOVE_H
 
-#endif
