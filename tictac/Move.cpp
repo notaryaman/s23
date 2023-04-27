0653b8d57b1201a5353577932c5ac383aa9f2d45
notaryaman <124849392+notaryaman@users.noreply.github.com>
2023-04-27 02:08:34 -0700

    Update Move.cpp

--- tictac/Move.cpp
+++ tictac/Move.cpp
@@ -1,26 +1,104 @@
+
+#include "Errors.h"
 #include "Move.h"
 #include <sstream>
-#include <stdexcept>
 #include <cctype>
+#include <iostream>
+using namespace std;
+// Space for implementing Move functions.
 
-Move::Move(const std::string& line) {
-    std::istringstream iss(line);
-    std::string square;
-    iss >> moveNumber >> player >> square;
-
-    if (iss.fail() || !iss.eof()) {
-        throw std::runtime_error("Parse error");
+Move::Move(const std::string& input)
+{
+  number = input.at(0);
+  player = input.at(2);
+  row = input.at(4);
+  column = input.at(5);
+  player = toupper(player);
+  int col = column;
+  size_t len = input.length();
+  if(!isspace(input.at(1)))
+     {
+        throw ParseError("Invalid structure");
+     }
+  if(!isspace(input.at(3)))
+     {
+        throw ParseError("Invalid structure");
+     }
+    
+  if(len<6)
+  {
+    throw ParseError("Invalid length");
+  }
+  if(len>6)
+  {
+    if(isspace(input.at(6)))
+    {
+      if(input.at(7) == '#')
+      {
+        len = len;
+      }
+      else
+      {
+        throw ParseError("Invalid comment");
+      }
     }
-
-    player = toupper(player);
-    square[0] = toupper(square[0]);
-    row = square[0] - 'A';
-    column = square[1] - '1';
+    else
+    {
+      throw ParseError("Invalid comment");
+    }
+  }
+  if(number<=9 && number>=1)
+  {
+          number=number;
+  }
+  else
+  {
+    throw ParseError("Invalid number");
+  }
+  if(player == 'O' || player == 'X')
+  {
+          player = player;
+  }
+  else
+  {
+          throw ParseError("Invalid player");
+  }
+  if(row>='A' && row<='C')
+  {
+          row = (row - 'A') + 1;
+  }
+   else
+  {
+          throw ParseError("Invalid row");
+  }
+  if(col>=1 &&col<= 3)
+  {
+       column = column;
+  }
+  else
+  {
+          throw ParseError("Invalid column");
+  }
+ /* if (!in.eof())
+  {
+    char nextChar;
+    char nxt;
+    in >> nextChar;
+    in >> nxt;
+    if (!isspace(nextChar))
+    {
+        throw ParseError("Invalid after column");
+    }
+    if(nxt != '#')
+    {
+      throw ParseError("Invalid after column");
+    }
+  }*/
 }
 
-bool Move::isValid(char expectedPlayer) const {
-    return moveNumber >= 1 && moveNumber <= 9 &&
-           player == expectedPlayer &&
-           row >= 0 && row < 3 &&
-           column >= 0 && column < 3;
+std::ostream& operator << (std::ostream& stream, const Move& move) 
+{
+  stream<<move.number<<' '<<move.player<<' ';
+  stream<< move.row<< move.column;
+  return stream;
 }
