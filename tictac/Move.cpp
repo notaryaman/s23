1a62c515ec78886fe87ab2d314e0576335615dc1
notaryaman <124849392+notaryaman@users.noreply.github.com>
2023-04-26 19:10:20 -0700

    Update Move.cpp

--- tictac/Move.cpp
+++ tictac/Move.cpp
@@ -16,10 +16,6 @@ Move::Move(const std::string& input)
   column = inp[5];
   player = toupper(player);
   int col = column;
-  if(inp.length()<6)
-  {
-    throw ParseError("Not long enough");
-  }
   if(number>9||number<1)
   {
           throw ParseError("Invalid number");
